f5c5e9f1bcff46ecce13699e1da60dac2059b8e6
Jan Stamer <jan.stamer@freenet.de>
2012-02-24 01:38:22 +0100

    First spring.

---
+++ kani-spring/src/main/java/org/kani/spring/ApplicationServlet.java
@@ -0,0 +1,47 @@
+package org.kani.spring;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.vaadin.Application;
+import com.vaadin.terminal.gwt.server.AbstractApplicationServlet;
+
+@SuppressWarnings("serial")
+public class ApplicationServlet extends AbstractApplicationServlet {
+	
+	private WebApplicationContext applicationContext;
+	
+	private Class<? extends Application> applicationClass;
+	
+	private String applicationBean;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void init(ServletConfig servletConfig) throws ServletException {
+		super.init(servletConfig);
+
+		applicationBean = servletConfig.getInitParameter("applicationBean");
+
+		if (applicationBean == null) {
+			throw new ServletException(
+					"ApplicationBean not specified in servlet parameters");
+		}
+
+		applicationContext = WebApplicationContextUtils.getWebApplicationContext(servletConfig.getServletContext());
+		applicationClass = (Class<? extends Application>) applicationContext.getType(applicationBean);
+	}
+
+	@Override
+	protected Class<? extends Application> getApplicationClass() throws ClassNotFoundException {
+		return applicationClass;
+	}
+
+	@Override
+	protected Application getNewApplication(HttpServletRequest request) {
+		return (Application) applicationContext.getBean(applicationBean);
+	}
+}
